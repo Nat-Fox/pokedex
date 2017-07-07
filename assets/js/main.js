af9866f82dede881c9a9b1ff97c543023ce2fc0f
Natalia Garrido <natgramirez@outlook.com>
2017-07-06 23:05:58 -0400

    probando llamado a la API

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // Llama end point de todos los pokemones
+    $.get('http://pokeapi.co/api/v2/pokedex/1/', function(result) {
+        console.log(result);
+        result.pokemon_entries.forEach(function(pokemon) {
+            $('.pokemon').append('<div>' + pokemon.pokemon_species.name + '</div>')
+        });
+
+        //console.log(result.pokemon_entries[0].pokemon_species.name);
+    });
+});
